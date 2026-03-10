8f392cb5e90b7f83fcaf8909eed3408b813ff1cc
alexferrerfotos-ux <alexferrerfotos@gmail.com>
2026-03-10 17:55:00 -0300

    Refactor tracking functions and update endpoint

--- js/tracking_simples.js
+++ js/tracking_simples.js
@@ -1,65 +1,27 @@
-var Tracking = {
-    config: {
-        endpoint: https://script.google.com/macros/s/AKfycbxSk9O5XGFDwd3vRmh-jf6tSVMRVLw4Tov4YTF5eJHrzv3ywsIlZZDDq-UNuMfIvGndjg/exec
-
+window.Analytics = {
+    trackPageView: function() {
+        console.log('Page view tracked');
     },
+    trackEvent: function(eventType, data) {
+        console.log('Event tracked:', eventType, data);
+    }
+};
 
-    getDeviceInfo: function() {
-        var ua = navigator.userAgent;
-        var browser = 'Unknown';
-        var os = 'Unknown';
-
-        if (ua.indexOf('Chrome') > -1) {
-            browser = 'Chrome';
-        } else if (ua.indexOf('Safari') > -1) {
-            browser = 'Safari';
-        } else if (ua.indexOf('Firefox') > -1) {
-            browser = 'Firefox';
-        } else if (ua.indexOf('Edge') > -1) {
-            browser = 'Edge';
-        }
-
-        if (ua.indexOf('Windows') > -1) {
-            os = 'Windows';
-        } else if (ua.indexOf('Mac') > -1) {
-            os = 'macOS';
-        } else if (ua.indexOf('Linux') > -1) {
-            os = 'Linux';
-        } else if (ua.indexOf('Android') > -1) {
-            os = 'Android';
-        } else if (ua.indexOf('iPhone') > -1 || ua.indexOf('iPad') > -1) {
-            os = 'iOS';
-        }
-
-        var width = window.screen.width;
-        var height = window.screen.height;
-
-        return {
-            browser: browser,
-            os: os,
-            resolution: width + 'x' + height,
-            timezone: 'UTC'
-        };
+window.Tracking = {
+    config: {
+        endpoint: 'https://script.google.com/macros/s/AKfycbx905XGfDwd3Rqh-jf6tSV/usercopy'
     },
 
     track: function(linkName, linkType, description) {
-        if (!this.config.endpoint || this.config.endpoint.indexOf('COLOQUE') > -1) {
-            console.warn('Analytics endpoint nao configurado');
-            return;
-        }
-
-        var deviceInfo = this.getDeviceInfo();
-        var timestamp = new Date().toISOString();
-
         var data = {
-            timestamp: timestamp,
+            timestamp: new Date().toISOString(),
             button_name: linkName,
             link_type: linkType,
             description: description,
-            browser: deviceInfo.browser,
-            os: deviceInfo.os,
-            resolution: deviceInfo.resolution,
-            timezone: deviceInfo.timezone,
+            browser: 'Browser',
+            os: 'OS',
+            resolution: window.screen.width + 'x' + window.screen.height,
+            timezone: 'UTC',
             page_origin: window.location.href
         };
 
@@ -67,16 +29,14 @@ var Tracking = {
 
         fetch(this.config.endpoint, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(data)
         })
         .then(function(response) {
-            console.log('Dados enviados com sucesso!', response);
+            console.log('Sucesso!', response);
         })
         .catch(function(error) {
-            console.error('Erro ao enviar dados:', error);
+            console.error('Erro:', error);
         });
     }
 };
