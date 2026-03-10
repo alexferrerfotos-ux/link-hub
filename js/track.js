6b198b69d6eb36f7a3481b684007dd0347aef1f7
alexferrerfotos-ux <alexferrerfotos@gmail.com>
2026-03-10 18:00:14 -0300

    Add tracking functionality to js/track.js

---
+++ js/track.js
@@ -0,0 +1,30 @@
+window.Tracking = {
+    config: {
+        endpoint: 'https://script.google.com/macros/s/AKfycbx905XGfDwd3Rqh-jf6tSV/usercopy'
+    },
+    track: function(linkName, linkType, description) {
+        var data = {
+            timestamp: new Date().toISOString(),
+            button_name: linkName,
+            link_type: linkType,
+            description: description,
+            browser: 'Browser',
+            os: 'OS',
+            resolution: window.screen.width + 'x' + window.screen.height,
+            timezone: 'UTC',
+            page_origin: window.location.href
+        };
+        console.log('Enviando para Analytics:', data);
+        fetch(this.config.endpoint, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+        .then(function(response) {
+            console.log('Dados enviados com sucesso!');
+        })
+        .catch(function(error) {
+            console.error('Erro ao enviar:', error);
+        });
+    }
+};
