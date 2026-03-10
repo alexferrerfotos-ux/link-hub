2f829372e6a4d1815effbb48e866113008a77bac
alexferrerfotos-ux <alexferrerfotos@gmail.com>
2026-03-10 18:12:36 -0300

    Implement tracking feature in Tracking object
    Added tracking functionality to send data to a specified endpoint.

--- js/track.js
+++ js/track.js
@@ -1,7 +1,37 @@
 window.Tracking = {
     config: {
         endpoint: https://script.google.com/macros/s/AKfycbxDdUgCRFtitXRP7NWILgbVFRciid8InrJt3SdKPFtxsZCk-0s5BJww6ljYL27QGxcz_g/exec
-    },
+window.Tracking = {
+        config: {
+                    endpoint: 'https://script.google.com/macros/s/AKfycbxDdUgCRftItXRP7NWlLgbVFRcid8lnrJt3SdKPFtxsZCk-0s5BJww6ijYL27QGx_g/usercopy'
+        },
+        track: function(linkName, linkType, description) {
+                    var data = {
+                                    timestamp: new Date().toISOString(),
+                                    button_name: linkName,
+                                    link_type: linkType,
+                                    description: description,
+                                    browser: 'Browser',
+                                    os: 'OS',
+                                    resolution: window.screen.width + 'x' + window.screen.height,
+                                    timezone: 'UTC',
+                                    page_origin: window.location.href
+                    };
+                    console.log('Enviando para Analytics:', data);
+                    fetch(this.config.endpoint, {
+                                    method: 'POST',
+                                    headers: {'Content-Type': 'application/json'},
+                                    body: JSON.stringify(data)
+                    })
+                    .then(function(response) {
+                                    console.log('Dados enviados com sucesso!');
+                    })
+                    .catch(function(error) {
+                                    console.error('Erro ao enviar:', error);
+                    });
+        }
+};
+},
     track: function(linkName, linkType, description) {
         var data = {
             timestamp: new Date().toISOString(),
